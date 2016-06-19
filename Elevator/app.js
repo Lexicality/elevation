4bc4562e3509f6d01a77989584dabfd74433d49a
lexicality <lex@lex.me.uk>
2016-06-19 14:14:35 +0000

    Compile app w/ new TS ver & config

--- Elevator/app.js
+++ Elevator/app.js
@@ -1,7 +1,3 @@
-﻿// Dummy IEFE due to reasons. Delete the first (
-(function () {
-})();
-
 var Elevation;
 (function (Elevation) {
     (function (Direction) {
@@ -12,45 +8,38 @@ var Elevation;
     var Direction = Elevation.Direction;
     function todir(direction) {
         if (direction == "up")
-            return 0 /* Up */;
+            return Direction.Up;
         else if (direction == "down")
-            return 2 /* Down */;
-        return 1 /* Resting */;
+            return Direction.Down;
+        return Direction.Resting;
     }
     function dirto(direction) {
-        if (direction == 0 /* Up */)
+        if (direction == Direction.Up)
             return "up";
-        else if (direction == 2 /* Down */)
+        else if (direction == Direction.Down)
             return "down";
         return "resting";
     }
-    var Elevator = (function () {
-        function Elevator(elevator, floors) {
+    class Elevator {
+        constructor(elevator, floors) {
             this.elevator = elevator;
             this.idle = false;
-            this.direction = 1 /* Resting */;
+            this.direction = Direction.Resting;
             // Where we're going to next
             this.destination = -1;
             // This assumes floor[0] = 0 and floor[n] = n
             var nfloors = floors.length;
             this.restingFloor = Math.floor(nfloors / 2);
-
             elevator.on("idle", this.onIdle.bind(this));
             elevator.on("floor_button_pressed", this.onFloorRequest.bind(this));
             elevator.on("passing_floor", this.onPassFloor.bind(this));
             elevator.on("stopped_at_floor", this.onArrive.bind(this));
         }
-        Object.defineProperty(Elevator.prototype, "floor", {
-            // The last floor the elevator went past. If stationary, the floor we're at.
-            get: function () {
-                return this.elevator.currentFloor();
-            },
-            enumerable: true,
-            configurable: true
-        });
-
-        Elevator.prototype.goToFloor = function (floor, override) {
-            if (typeof override === "undefined") { override = false; }
+        // The last floor the elevator went past. If stationary, the floor we're at.
+        get floor() {
+            return this.elevator.currentFloor();
+        }
+        goToFloor(floor, override = false) {
             if (this.idle)
                 override = true;
             if (!override && this.elevator.destinationQueue.indexOf(floor) !== -1)
@@ -59,10 +48,11 @@ var Elevation;
                 this.setDestination(floor);
                 console.warn("Going to %d directly", floor);
                 this.elevator.goToFloor(floor, true);
-            } else {
+            }
+            else {
                 console.info("Inserting %d into the queue", floor);
                 this.elevator.destinationQueue.push(floor);
-                if (this.direction == 0 /* Up */)
+                if (this.direction == Direction.Up)
                     this.elevator.destinationQueue.sort(this.sortUp.bind(this));
                 else
                     this.elevator.destinationQueue.sort(this.sortDown.bind(this));
@@ -71,33 +61,30 @@ var Elevation;
                 this.setDestination(this.elevator.destinationQueue[0]);
             }
             this.idle = false;
-        };
-        Elevator.prototype.requestGoingUp = function (floor) {
+        }
+        requestGoingUp(floor) {
             console.info("Request to go up to floor %d!", floor);
             this.goToFloor(floor);
-        };
-        Elevator.prototype.requestGoingDown = function (floor) {
+        }
+        requestGoingDown(floor) {
             console.info("Request to go down to floor %d!", floor);
             this.goToFloor(floor);
-        };
-
-        Elevator.prototype.setDestination = function (floor) {
+        }
+        setDestination(floor) {
             if (floor == this.floor)
-                this.direction = 1 /* Resting */;
+                this.direction = Direction.Resting;
             else if (floor > this.floor)
-                this.direction = 0 /* Up */;
+                this.direction = Direction.Up;
             else
-                this.direction = 2 /* Down */;
+                this.direction = Direction.Down;
             var e = this.elevator;
-
             // Indicators turned off due to passengers
             //e.goingDownIndicator(this.direction == Direction.Down);
             //e.goingUpIndicator(this.direction == Direction.Up);
             this.destination = floor;
             console.info("Destination is now %d, we are at %d. We are going %s!", floor, this.floor, dirto(this.direction));
-        };
-
-        Elevator.prototype.sortUp = function (a, b) {
+        }
+        sortUp(a, b) {
             var f = this.floor;
             if (a < f) {
                 if (b > f)
@@ -105,89 +92,72 @@ var Elevation;
                 if (b > a)
                     return 1;
                 return -1;
-            } else if (a < b) {
+            }
+            else if (a < b) {
                 return -1;
             }
             return 1;
-        };
-        Elevator.prototype.sortDown = function (a, b) {
+        }
+        sortDown(a, b) {
             var f = this.floor;
             if (b > f) {
                 if (a < f)
                     return -1;
                 if (a < b)
                     return -1;
-            } else if (a > b) {
+            }
+            else if (a > b) {
                 return -1;
             }
             return 1;
-        };
-
-        Elevator.prototype.onIdle = function () {
+        }
+        onIdle() {
             console.info("Idle!");
             if (!this.idle) {
                 this.goToFloor(this.restingFloor);
                 this.idle = true;
             }
-        };
-        Elevator.prototype.onFloorRequest = function (floor) {
+        }
+        onFloorRequest(floor) {
             console.info("Internal floor req for %d", floor);
-
             // TODO
             this.goToFloor(floor);
-        };
-        Elevator.prototype.onPassFloor = function (floor, _direction) {
+        }
+        onPassFloor(floor, _direction) {
             console.log("Passing floor %d going %s", floor, _direction);
             var direction = todir(_direction);
             //this.floor = floor;
             // TODO
-        };
-        Elevator.prototype.onArrive = function (floor) {
+        }
+        onArrive(floor) {
             console.log("Arrived at floor %s", floor);
-
             //this.floor = floor;
             if (this.elevator.destinationQueue.length > 0) {
                 this.destination = this.elevator.destinationQueue[0];
                 this.setDestination(this.destination);
-                // todo
             }
             // todo
-        };
-        return Elevator;
-    })();
-
-    var ElevatorControl = (function () {
-        function ElevatorControl() {
+        }
+    }
+    class ElevatorControl {
+        constructor() {
             this.elevators = [];
         }
-        ElevatorControl.prototype.init = function (elevators, floors) {
-            var _this = this;
-            elevators.forEach(function (elevator) {
-                _this.elevators.push(new Elevator(elevator, floors));
+        init(elevators, floors) {
+            elevators.forEach((elevator) => {
+                this.elevators.push(new Elevator(elevator, floors));
             });
-
             // FIXME
-            floors.forEach(function (floor, i) {
-                floor.on("up_button_pressed", function () {
-                    return _this.elevators.forEach(function (elevator) {
-                        return elevator.requestGoingUp(i);
-                    });
-                });
-                floor.on("down_button_pressed", function () {
-                    return _this.elevators.forEach(function (elevator) {
-                        return elevator.requestGoingDown(i);
-                    });
-                });
+            floors.forEach((floor, i) => {
+                floor.on("up_button_pressed", () => this.elevators.forEach((elevator) => elevator.requestGoingUp(i)));
+                floor.on("down_button_pressed", () => this.elevators.forEach((elevator) => elevator.requestGoingDown(i)));
             });
-        };
-        ElevatorControl.prototype.update = function (dt, elevators, floors) {
+        }
+        update(dt, elevators, floors) {
             // We normally don't need to do anything heree
-        };
-        return ElevatorControl;
-    })();
+        }
+    }
     Elevation.ElevatorControl = ElevatorControl;
 })(Elevation || (Elevation = {}));
-
 var test;
 test = new Elevation.ElevatorControl();
-// Delete the last )
